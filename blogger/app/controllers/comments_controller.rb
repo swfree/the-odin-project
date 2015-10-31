997558f9a35bf96b9e07149a62ab48273bfd7ace
swfree <skye.free@gmail.com>
2015-10-30 23:21:45 -0400

    upload blogger project

---
+++ blogger/app/controllers/comments_controller.rb
@@ -0,0 +1,21 @@
+class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.article_id = params[:article_id]
+
+    @comment.save
+    redirect_to article_path(@comment.article)
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+
+    @comment.destroy
+
+    flash.notice = "Comment Deleted."
+  end
+
+  def comment_params
+    params.require(:comment).permit(:author_name, :body)
+  end
+end
